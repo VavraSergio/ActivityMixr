089dd0cb156c7d7e44e85d134b31b8c9b510ed62
Sergio Vavra <Vavracs@dukes.jmu.edu>
2024-04-30 17:15:08 -0400

    Started the BoredAPI generate page.

---
+++ scripts/generate.js
@@ -0,0 +1,24 @@
+document.getElementById('activityForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    let participants = document.getElementById('participants').value;
+
+    if (participants === '' || parseInt(participants) < 1) {
+        participants = 1;
+    }
+
+    let type = document.getElementById('type').value;
+
+
+    const apiUrl = 'https://www.boredapi.com/api/activity?' + 'participants=' + participants + '&' + 'type=' + type;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('activity').textContent = data.activity;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+});
