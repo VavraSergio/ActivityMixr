73a744e37e6b5a5cb09e96c1ae841dd6928e5ce9
Sergio Vavra <Vavracs@dukes.jmu.edu>
2024-04-30 18:00:34 -0400

    Fiddling with boredAPI

--- scripts/generate.js
+++ scripts/generate.js
@@ -10,8 +10,39 @@ document.getElementById('activityForm').addEventListener('submit', function (eve
 
     let type = document.getElementById('type').value;
 
+    let accessibility = document.getElementById('accessibility').value;
 
-    const apiUrl = 'https://www.boredapi.com/api/activity?' + 'participants=' + participants + '&' + 'type=' + type;
+    let price = document.getElementById('price').value;
+
+    let apiURL;
+    if (type === 'Any') {
+        apiUrl = 'https://www.boredapi.com/api/activity' + 'participants=' + participants + '&price=' + price + '&accessibility=' + accessibility;
+    } else {
+        apiUrl = 'https://www.boredapi.com/api/activity' + 'participants=' + participants + '&type=' + type + '&price=' + price + '&accessibility=' + accessibility;
+    }
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('activity').textContent = data.activity;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+});
+
+document.getElementById('accessibility').addEventListener('input', function () {
+    document.getElementById('accessibilityValue').textContent = this.value;
+});
+
+document.getElementById('price').addEventListener('input', function () {
+    document.getElementById('priceValue').textContent = this.value;
+});
+
+document.getElementById('activityForm').addEventListener('button', function (event) {
+    event.preventDefault();
+
+    apiUrl = 'https://www.boredapi.com/api/activity';
 
     fetch(apiUrl)
         .then(response => response.json())
