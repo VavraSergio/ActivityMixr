c3711731a2f4a44956e28d7e402f2e01b9f559fc
AzureJMU <127335749+AzureJMU@users.noreply.github.com>
2024-04-30 19:58:13 -0400

    due to thunderstom and fear of losing power, I am committing this

---
+++ scripts/spotifyAPI.js
@@ -0,0 +1,8 @@
+const clientId = 'd774e69457df4fafbd90d6fb8208be05'
+const redirectUri = 'https://wanghci.github.io/project-milestone-2-team-azuresergio/generate.html'
+
+document.getElementById('spotify-login').addEventListener('submit', function ( event ) {
+    event.preventDefault();
+
+    request
+})
