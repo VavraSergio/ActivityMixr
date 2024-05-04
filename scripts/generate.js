dda2513a0875d9699e190d81d0a51fd5d30f89e0
AzureJMU <127335749+AzureJMU@users.noreply.github.com>
2024-05-03 20:53:51 -0400

    playlist.images['url']

--- scripts/generate.js
+++ scripts/generate.js
@@ -185,6 +185,7 @@ document.getElementById('lucky').addEventListener('click', function (event) {
                 localStorage.setItem("playlist", JSON.stringify(playlist));
                 localStorage.setItem("playlistID", playlist.id);
                 localStorage.setItem("playlist-description", playlist.description);
+                console.log(playlist.images['url'])
                 console.log("Playlist generated successfully:", playlist);
             } catch (error) {
                 console.error('Error generating playlist:', error);
