2b1ee9a16e5b2b7cf52666458ec6b03310e1d716
AzureJMU <127335749+AzureJMU@users.noreply.github.com>
2024-05-03 21:50:32 -0400

    got rid of image

--- scripts/generate.js
+++ scripts/generate.js
@@ -183,8 +183,6 @@ document.getElementById('lucky').addEventListener('click', function (event) {
                 const responseData = await response.json();
                 const playlist = responseData.playlists.items[0];
 
-                const playlistImage = playlist.images[0].url
-                localStorage.setItem("playlist-image", JSON.stringify(playlistImage))
                 // const playlistImg = playlist.images[ 'url' ]
                 // const imgResponse = await fetch(playlistImg, payload)
                 // const imgResponseData = await imgResponse.json()
