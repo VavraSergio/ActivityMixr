b9afc51835ee8ef1d9a7613f7ef97359a0d335e8
AzureJMU <127335749+AzureJMU@users.noreply.github.com>
2024-05-03 23:23:22 -0400

    double-checking to see if playlist link is messing up the code

--- scripts/generate.js
+++ scripts/generate.js
@@ -185,7 +185,7 @@ document.getElementById('lucky').addEventListener('click', function (event) {
                 const imageUrls = playlistObj.images.map(image => image.url)
                 localStorage.setItem("image-url", imageUrls)
                 localStorage.setItem("playlistObj", JSON.stringify(playlistObj));
-                localStorage.setItem( "playlist", playlistObj.external_urls.spotify )
+                // localStorage.setItem( "playlist", playlistObj.external_urls.spotify )
                 localStorage.setItem("playlistID", playlistObj.id);
                 localStorage.setItem("playlist-description", playlistObj.description);
                 console.log("Playlist generated successfully:", playlistObj);
